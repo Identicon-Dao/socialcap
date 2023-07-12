43f3487dcb1390778dcda70ac443c9c67ebf55a0
Nicolas Massi <massinicolas@gmail.com>
2023-07-12 14:29:26 +0200

    css files cleanup

--- frontend/zkapp/src/routes/+layout.svelte
+++ frontend/zkapp/src/routes/+layout.svelte
@@ -1,23 +1,23 @@
 <script>
-  import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-	import { getItem } from '$lib/utility/localStorageController';
-	import { page } from '$app/stores';
-  
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+  import { getItem } from "$lib/utility/localStorageController";
+  import { page } from "$app/stores";
+
   // local imports ...
-  import { Styles } from 'sveltestrap';
-  import RootHeader from '@components/RootHeader.svelte';
+  import { Styles } from "sveltestrap";
+  import RootHeader from "@components/RootHeader.svelte";
 
   // Hexui
-  let isAuthenticated = getItem('access_token');
-	let currentPage = $page.url.pathname;
+  let isAuthenticated = getItem("access_token");
+  let currentPage = $page.url.pathname;
 
   onMount(async () => {
-		if (isAuthenticated && isAuthenticated !== '') {
-  			goto(currentPage);
-  		} else {
-  			goto(`/signin`);
-  		}   
+    if (isAuthenticated && isAuthenticated !== "") {
+      goto(currentPage);
+    } else {
+      goto(`/signin`);
+    }
   });
 </script>
 
@@ -25,19 +25,17 @@
 -->
 <Styles />
 
-<!--
-  MINA original not used here
--->
-<style global>
-  @import '/css/mina-fonts.css';
-  /* @import '/css/bootstrap.css'; */
-  @import '/css/custom.css';
-</style>
-
 <svelte:head>
   <title>Socialcap</title>
 </svelte:head>
 
-<div style="position:fixed;top:0;left:0;bottom:0;right:0;">
-  <slot></slot>
+<div>
+  <slot />
 </div>
+
+<!--
+  MINA original not used here
+-->
+<style global>
+  @import "/css/custom.min.css";
+</style>
