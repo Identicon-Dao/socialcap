c5d4b616946a85619da38fda8222dd26477c7147
leomanza <leomanzanal@gmail.com>
2024-04-07 15:13:49 -0300

    community page fetchs refactor

--- frontend/zkapp/src/routes/(detail)/community/[uid]/+page.js
+++ frontend/zkapp/src/routes/(detail)/community/[uid]/+page.js
@@ -1,8 +1,6 @@
 
 import { error } from '@sveltejs/kit';
-import { getCommunity, getMyClaims, getMyCredentials } from '@apis/queries';
-import { APPROVED } from "@models/states";
-import { getCurrentUser } from '@models/current-user';
+import { getCommunity} from '@apis/queries';
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
 
@@ -10,13 +8,6 @@ export async function load({ params }) {
         let obj = await getCommunity(params.uid);
         if (!obj)
             throw error(404, 'Community Not found');
-        const claimed = await getMyClaims(params);
-        let user = await getCurrentUser();
-        const credentials = await getMyCredentials({user, communityUid: params.uid});
-        obj.claimed = claimed;
-        obj.credentials = credentials;
-        obj.approvedClaims = obj.claims.filter((c) => c.state == APPROVED);
-        obj.claimables = []; // await getClaimables(params.uid)
         return JSON.parse(JSON.stringify(obj));
     }
     throw error(404, 'Not found');
