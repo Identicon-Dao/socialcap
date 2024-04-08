d1c80380d7ec1091d7abfa2185e16b301919673e
leomanza <leomanzanal@gmail.com>
2024-04-08 10:55:51 -0300

    refactor fetch data on credential claimed detail & credential edit

--- frontend/zkapp/src/routes/(detail)/credential/claimed/[uid]/+page.js
+++ frontend/zkapp/src/routes/(detail)/credential/claimed/[uid]/+page.js
@@ -1,33 +1,13 @@
 import { error } from '@sveltejs/kit';
-import { getPlan, getCommunity, getClaim } from "@apis/queries";
-import { getCurrentUser } from '@models/current-user';
-
-
-async function loadClaim(claimUid) {
-  let claim = await getClaim(claimUid);
-
-  const plan = await getPlan(claim.planUid);
-
-  const org = await getCommunity(claim.communityUid);
-
-  claim.community = org.name;
-  claim.type = plan.name;
-  claim.description = plan.description;
-  claim.image = plan.image; 
-
-  return claim; 
-}
-
+import { getClaim } from "@apis/queries";
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, route, url }) {
   if (params.uid !== "") {
-      let aClaim = await loadClaim(params.uid);
-      if (!aClaim)
+    let claim = await getClaim(claimUid);
+      if (!claim)
         throw error(404, 'Claim Not found');
-      aClaim.evidenceData = typeof(aClaim.evidenceData) === 'string'
-        ? JSON.parse(aClaim.evidenceData)
-        : aClaim.evidenceData;
+    
       return JSON.parse(JSON.stringify(aClaim));
     }
     throw error(404, 'Not found');
--- frontend/zkapp/src/routes/(detail)/credential/edit/[uid]/+page.js
+++ frontend/zkapp/src/routes/(detail)/credential/edit/[uid]/+page.js
@@ -3,90 +3,22 @@
  *  slug uid: the UID of the Master Plan
  */
 import { error } from '@sveltejs/kit';
-import { UID } from "@utilities/uid";
-import { getPlan, getCommunity, getClaim } from "@apis/queries";
-import { getCurrentUser } from '@models/current-user';
-import { fixEvidenceData } from './fix-evidence-data';
-
-async function initNewClaim(planUid, user) {
-  const plan = await getPlan(planUid);
-
-  const community = await getCommunity(plan.communityUid);
-
-  let oClaim = {
-    uid: UID.uuid4(),
-    communityUid: plan.communityUid,
-    planUid: plan.uid,
-    applicantUid: user.uid,
-    accountId: "",
-    // derived form MasterPlan name for this credential
-    type: plan.name, 
-    description: plan.description,
-    state: 1, // DRAFT
-    community: community.name,
-    image: plan.image, 
-    // activity times
-    createdUTC: null,
-    updatedUTC: null,
-    votedUTC: null,
-    issuedUTC: null,
-    dueUTC: plan.dueUTC,
-    // voting results
-    requiredVotes: plan.requiredVotes, // copied from MasterPlan
-    requiredPositives: plan.requiredPositives,
-    positiveVotes: 0,
-    negativeVotes: 0,
-    ignoredVotes: 0,
-    // evidence data
-    evidenceData: (plan.evidence || []).map((f) => {
-      f.value = "";
-      return f;
-    })
-  };
-
-  return { 
-    claim: oClaim, 
-    plan: plan,
-    community: community,
-    isNew: true 
-  }; 
-}
-
-
-async function loadClaim(claimUid) {
-  let claim = await getClaim(claimUid);
-  if (!claim)
-    throw error(404, 'Claim Not found');
-  const plan = await getPlan(claim.planUid);
-
-  const org = await getCommunity(claim.communityUid);
-
-  claim.community = org.name;
-  claim.type = plan.name;
-  claim.description = plan.description;
-  claim.image = plan.image; 
-  claim.evidenceData = fixEvidenceData(plan.evidence, claim.evidenceData);
-
-  return { 
-    claim: claim, 
-    plan: plan,
-    community: org,
-    isNew: false 
-  }; 
-}
-
+import { getClaim } from "@apis/queries";
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, route, url }) {
 
   if (params.uid !== "") {
-      let user = await getCurrentUser();
       const isNew = url.searchParams.get('isnew', null);
+      if (isNew === null) {
+        let claim = await getClaim(claimUid);
+        if (!claim)
+          throw error(404, 'Claim Not found');
+        return {isNew: false, claimUid: params.uid, claim}
+      }
 
-      return ((isNew !== null)
-        ? await initNewClaim(params.uid, user) 
-        : await loadClaim(params.uid)
-      )  
+      return {isNew: true, planUid: params.uid}
     }
-    throw error(500, 'Ndsdsdot found');
+    throw error(400, 'Not found');
+
 }
