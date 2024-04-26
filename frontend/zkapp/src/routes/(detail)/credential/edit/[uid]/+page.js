651f1ac150d8e28db656f5c49293dc0bca5e42c0
leomanza <leomanzanal@gmail.com>
2024-04-26 17:16:51 -0300

    fix not authenticated claim

--- frontend/zkapp/src/routes/(detail)/credential/edit/[uid]/+page.js
+++ frontend/zkapp/src/routes/(detail)/credential/edit/[uid]/+page.js
@@ -4,21 +4,30 @@
  */
 import { error } from '@sveltejs/kit';
 import { getClaim } from "@apis/queries";
-
+import { getCurrentSession } from '@models/current-session';
+import { goto } from '$app/navigation';
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, route, url }) {
+  // check logged in 
+  // const user = await getCurrentUser();
+  // console.log("user", user)
+  let isAuthenticated = getCurrentSession();
+  console.log("is auth?", isAuthenticated)
+  if (!isAuthenticated) {
+
+    goto("/login");
+  }
 
   if (params.uid !== "") {
-      const isNew = url.searchParams.get('isnew', null);
-      if (isNew === null) {
-        let claim = await getClaim(params.uid);
-        if (!claim)
-          throw error(404, 'Claim Not found');
-        return {isNew: false, claimUid: params.uid, claim}
-      }
-
-      return {isNew: true, planUid: params.uid, claim: null}
+    const isNew = url.searchParams.get('isnew', null);
+    if (isNew === null) {
+      let claim = await getClaim(params.uid);
+      if (!claim)
+        throw error(404, 'Claim Not found');
+      return { isNew: false, claimUid: params.uid, claim }
     }
-    throw error(400, 'Not found');
 
+    return { isNew: true, planUid: params.uid, claim: null }
+  }
+  throw error(400, 'Not found');
 }
